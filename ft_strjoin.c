1ef187360a8e0df2be95e1af6522bb639d13e1fa
joao-per <rodriguespereirajoaopaulo@gmail.com>
2022-09-10 21:06:00 +0100

    done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,32 @@
 /*   By: joao-per <joao-per@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 18:23:15 by joao-per          #+#    #+#             */
-/*   Updated: 2022/09/05 18:23:16 by joao-per         ###   ########.fr       */
+/*   Updated: 2022/09/10 21:04:19 by joao-per         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	a;
+	size_t	len;
+	char	*result;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	result = (char *)malloc(len + 1);
+	if (!result)
+		return (0);
+	a = 0;
+	while (s1[a])
+		result[i++] = s1[a++];
+	a = 0;
+	while (s2[a])
+		result[i++] = s2[a++];
+	result[i] = '\0';
+	return (result);
+}
